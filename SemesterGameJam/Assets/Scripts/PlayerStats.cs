3fc342a4b8225ce71d1c13adf7807ab697639d64
Lerkus <Sebastian-Maag@gmx.de>
2016-12-10 00:34:02 +0100

    shotgun functional

--- SemesterGameJam/Assets/Scripts/PlayerStats.cs
+++ SemesterGameJam/Assets/Scripts/PlayerStats.cs
@@ -3,7 +3,11 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class PlayerStats : Stats {
-
+    [Header("Attack Stats")]
+    public float attackCoolDown = 0.5f;
+    public int shotsToFire = 5;
+    public float shotSpeed = 10;
+    public float shotgunWidthModifier = 0.1f;
 
     public override void die()
     {
