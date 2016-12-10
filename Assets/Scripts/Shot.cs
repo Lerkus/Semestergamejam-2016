b1cca576f7a11b9f8293ce6763cb934f32e34b85
Lerkus <Sebastian-Maag@gmx.de>
2016-12-10 13:21:22 +0100

    Prefab overhault

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -6,11 +6,13 @@ public class Shot : MonoBehaviour {
     public int damage = 1;
     public float timeToLive = 0.2f;
     private Coroutine timer;
+    public Sprite[] shots = new Sprite[4];
 
 
     public void Awake()
     {
         timer = StartCoroutine(timeOut());
+        gameObject.GetComponent<SpriteRenderer>().sprite = shots[Mathf.RoundToInt(Random.value * 4.0f)];
     }
 
     public void OnTriggerEnter2D(Collider2D coll)
