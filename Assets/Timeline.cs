741621dc7c290b441c653b43190967a884d3d749
Jay <j4y@mail.de>
2016-12-10 21:25:19 +0100

    Timeline ohne Health

---
+++ Assets/Timeline.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Timeline : MonoBehaviour {
+	public int progress = 0;
+	public int offset = 0;
+	public Image TimelineImg;
+	public Sprite[] SpriteBatch;
+
+	private int LastProgress;
+
+	// Use this for initialization
+	void Start () {
+		LastProgress = progress;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (LastProgress != progress) {
+			LastProgress = progress;
+
+			foreach(var sprite in SpriteBatch)
+			{
+				var i = 0;
+
+				int.TryParse (sprite.name, out i);
+
+				if(i == Mathf.Max(1, Mathf.Min(40, progress - offset)))
+				{
+					TimelineImg.sprite = sprite;
+					break;
+				}
+			}
+		}
+	}
+}
