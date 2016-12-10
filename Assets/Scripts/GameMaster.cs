64dc2960e54963356f0b9fefe5272e5ada237b50
Lerkus <Sebastian-Maag@gmx.de>
2016-12-10 02:38:58 +0100

    Wavemanager and Gamemaster introduced

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameMaster : MonoBehaviour {
+    private static GameMaster masterReferenz;
+
+    public static GameMaster getGameMaster()
+    {
+        return masterReferenz;
+    }
+
+	void Awake () {
+        masterReferenz = this;
+	}
+    void Start()
+    {
+        gameObject.GetComponent<WaveManager>().nextWave();
+    }
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class WaveManager : MonoBehaviour
+{
+    public Vector2 minBounds = new Vector2(0, 0);
+    public Vector2 maxBounds = new Vector2(10, 10);
+    public float distanceToMid = 10;
+    public float timeDelayBetweenWaves = 60;
+    private IEnumerator timer;
+    private int actualWave = 0;
+    public int maxWave = 5;
+    public GameObject EnemyPrefab;
+    public List<int> amountOfEnemiesToSpawn;
+    public int difficulty = 10;
+
+    public void Start()
+    {
+        amountOfEnemiesToSpawn = new List<int>(maxWave);
+
+    }
+
+    private IEnumerator waitingForNextWave()
+    {
+        yield return new WaitForSeconds(timeDelayBetweenWaves);
+        StopCoroutine(timer);
+        nextWave();
+    }
+
+    public void nextWave()
+    {
+        if (actualWave < maxWave)
+        {
+            spawnWave();
+            StartCoroutine(waitingForNextWave());
+            actualWave++;
+        }
+    }
+
+    private void spawnWave()
+    {
+        Vector2 placeOfSpawning;
+        for (int i = 0; i < amountOfEnemiesToSpawn[actualWave]; i++)
+        {
+            placeOfSpawning = (minBounds + maxBounds) / 2 + Random.insideUnitCircle.normalized * distanceToMid;
+            Instantiate(EnemyPrefab, placeOfSpawning, new Quaternion());
+        }
+    }
+}
