a518ef23cf923a3e3d6a4189813b020879f0301b
Jay <j4y@mail.de>
2016-12-10 22:06:37 +0100

    Health Glocken

---
+++ Assets/HealthScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour {
+
+	public GameObject[] Glocke;
+	public int Health=12;
+	private int LastHealth;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+		LastHealth = Health;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (LastHealth != Health) {
+			LastHealth = Health;
+
+			for (var i = 0; i < Glocke.Length; i++) {
+			
+				Glocke [i].SetActive (i < Health);
+			}
+		}
+
+
+	
+	}
+}
