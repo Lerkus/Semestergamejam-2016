968902c7e43fdc458f6f03e739b99169a4ec4f17
zengerle <ga42zax@mytum.de>
2016-12-11 10:52:39 +0100

    resolved conflicts

--- Assets/Scripts/PlayerContoller.cs
+++ Assets/Scripts/PlayerContoller.cs
@@ -13,11 +13,7 @@ public class PlayerContoller : MonoBehaviour {
 
 	void Start() {
 		rg = transform.parent.GetComponent<Rigidbody2D> ();
-<<<<<<< HEAD
 		canvas = (Canvas) Object.FindObjectOfType<Canvas> ();
-=======
-		//canvas = GameObject.Find ("Canvas");
->>>>>>> 9b15fa910e80020fd60b9a9d184a9770acd5c9d1
 	}
 
 	void FixedUpdate() {
